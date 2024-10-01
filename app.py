17bc11ecf7bd890e8d1b65a3b30cd5ad8bde2bc3
Rumit Shah <rumitshahn@gmail.com>
2024-10-02 00:25:19 +0530

    Used GET to call all data of users

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
+import json
 
 obj = Flask(__name__)
 
 @obj.route('/v1/users')
-def hello_world():
-    return 'Hello World!'
+def Users():
+    with open('database.json', 'r') as file:
+        data = json.load(file)
+    return jsonify(data)
     
 if __name__ == "__main__":
     obj.run(debug=True)

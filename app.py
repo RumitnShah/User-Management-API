d668cad9d1fae2f6b5b17f69347b5dc24497cc43
Rumit Shah <rumitshahn@gmail.com>
2024-10-06 17:27:09 +0530

    Modified route for different operations

--- app.py
+++ app.py
@@ -3,37 +3,37 @@ import json
 
 obj = Flask(__name__)
 
-# @obj.route('/v1/users')  ##To get all the users.
-# def Users():
-#     with open('database.json', 'r') as file:
-#         data = json.load(file)
-#     return jsonify(data)
+@obj.route('/v1/users', methods=['GET'])     #To get all the users.
+def Users():
+    with open('database.json', 'r') as file:
+        data = json.load(file)
+    return jsonify(data)
 
-# @obj.route('/v1/users/ID')   # To get users with specific id only
-# def user_id():
-#     user_id = request.args.get('id')
+@obj.route('/v1/users/ids', methods=['GET'])     # To get users with specific id only
+def user_id():
+    user_id = request.args.get('id')
     
-#     if not user_id:
-#         return jsonify({"error": "No user ID provided"})
+    if not user_id:
+        return jsonify({"error": "No user ID provided"})
 
-#     with open('database.json', 'r') as file:
-#         data = json.load(file)
+    with open('database.json', 'r') as file:
+        data = json.load(file)
 
-#     users = data.get("users", [])
+    users = data.get("users", [])
     
-#     for user in users:
-#         if user["id"] == user_id:
-#             return jsonify({
-#                 "id": user["id"],
-#                 "name": user["name"],
-#                 "age": user["age"],
-#                 "team": user["team"]
-#             })
+    for user in users:
+        if user["id"] == user_id:
+            return jsonify({
+                "id": user["id"],
+                "name": user["name"],
+                "age": user["age"],
+                "team": user["team"]
+            })
     
-# if __name__ == "__main__":
-#     obj.run(debug=True)
+if __name__ == "__main__":
+    obj.run(debug=True)
 
-@obj.route('/v1/users/teams')     #To get users with same team code
+@obj.route('/v1/users/teams', methods=['GET'])       #To get users with same team code
 def user_team():
     user_team = request.args.get('team')
     

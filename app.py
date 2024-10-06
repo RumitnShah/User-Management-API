81e06bfffb51d9406e7940f5fb4da2eac35320b1
Rumit Shah <rumitshahn@gmail.com>
2024-10-06 17:09:53 +0530

    Finding users with same team code

--- app.py
+++ app.py
@@ -3,34 +3,63 @@ import json
 
 obj = Flask(__name__)
 
-# @obj.route('/v1/users')
+# @obj.route('/v1/users')  ##To get all the users.
 # def Users():
 #     with open('database.json', 'r') as file:
 #         data = json.load(file)
 #     return jsonify(data)
 
-@obj.route('/v1/users')
-def user_id():
-    user_id = request.args.get('id')
+# @obj.route('/v1/users/ID')   # To get users with specific id only
+# def user_id():
+#     user_id = request.args.get('id')
     
-    if not user_id:
-        return jsonify({"error": "No user ID provided"})
+#     if not user_id:
+#         return jsonify({"error": "No user ID provided"})
+
+#     with open('database.json', 'r') as file:
+#         data = json.load(file)
+
+#     users = data.get("users", [])
+    
+#     for user in users:
+#         if user["id"] == user_id:
+#             return jsonify({
+#                 "id": user["id"],
+#                 "name": user["name"],
+#                 "age": user["age"],
+#                 "team": user["team"]
+#             })
+    
+# if __name__ == "__main__":
+#     obj.run(debug=True)
+
+@obj.route('/v1/users/teams')     #To get users with same team code
+def user_team():
+    user_team = request.args.get('team')
+    
+    if not user_team:
+        return jsonify({"error": "No user team provided"})
 
     with open('database.json', 'r') as file:
         data = json.load(file)
 
     users = data.get("users", [])
-    
-    for user in users:
-        if user["id"] == user_id:
-            return jsonify({
-                "id": user["id"],
-                "name": user["name"],
-                "age": user["age"],
-                "team": user["team"]
-            })
-    
-    return jsonify({"error": "User not found"}), 404
 
+    same_code_team = []
+
+    for user in users:
+        if user["team"] == user_team:
+            same_code_team.append({
+                                        "id": user["id"],
+                                        "name": user["name"],
+                                        "age": user["age"],
+                                        "team": user["team"]
+                                    })
+    if same_code_team:
+        return jsonify(same_code_team)
+    
+    else:
+        return jsonify({"Error": "No team with given code found"})
+    
 if __name__ == "__main__":
     obj.run(debug=True)
